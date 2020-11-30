6f1de896b1c5c8113a397bbd8aae7936aa56e303
adrien341 <68065056+adrien341@users.noreply.github.com>
2020-11-30 17:24:55 +0100

    Hey first try of commit

---
+++ Code_1.py
@@ -0,0 +1,10 @@
+# ici je vais juste te réexpliquer comment créer un environnement virtuel
+# pythhon3 -m venv myvenvName
+# pip freeze
+# venv\Scripts\Activate
+# deactivate
+# pip freeze > requirements.txt #pour garder en mémoire les packages à installer
+
+
+# au retour : 
+# pip install -r requirements.txt
